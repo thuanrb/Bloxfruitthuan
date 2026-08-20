4272f3791e291026eb6560bc992a63f5e144826d
thuanrb <khadza2k45@gmail.com>
2026-08-20 21:00:42 +0700

    Update Combat.lua

--- Modules/Combat.lua
+++ Modules/Combat.lua
@@ -9,15 +9,26 @@ function CombatModule:Init(Hub)
         while task.wait() do
             if Hub.Config.FastAttack then
                 pcall(function()
-                    if LocalPlayer.Character and LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-                        local Combat = LocalPlayer.Character:FindFirstChildOfClass("Tool")
-                        if Combat and Combat:FindFirstChild("Damage") then
-                            Combat:Activate()
+                    local character = LocalPlayer.Character
+                    if character and character:FindFirstChild("HumanoidRootPart") then
+                        local tool = character:FindFirstChildOfClass("Tool")
+                        if tool and tool:FindFirstChild("Damage") then
+                            tool:Activate()
                             VirtualUser:Button1Down(Vector2.new(1, 1))
                             task.wait(0.01)
                             VirtualUser:Button1Up(Vector2.new(1, 1))
                         end
                     end
+                end)
+            end
+        end
+    end)
+
+    task.spawn(function()
+        while task.wait(0.5) do
+            if Hub.Config.BringMobs then
+                pcall(function()
+                    -- Logic Bring Mobs (Gom quái lại gần người chơi)
                 end)
             end
         end
