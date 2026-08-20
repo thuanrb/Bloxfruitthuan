dc9631c80c91d6d96b0f4cb3ad8b0e4435be249f
thuanrb <khadza2k45@gmail.com>
2026-08-20 22:44:28 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,22 +1,10 @@
 local Loader = {}
 Loader.Config = { AutoFarm = false, AutoFarmNearest = false, AutoStats = false, FastAttack = false, BringMobs = false, ESPEnabled = false, ChestESP = false, BoostFPS = false, HopTTK = false, SelectedWeapon = "Melee", SelectedStat = "Melee" }
-Loader.Services = { Players = game:GetService("Players"), Workspace = game:GetService("Workspace"), RunService = game:GetService("RunService"), VirtualUser = game:GetService("VirtualUser"), TweenService = game:GetService("TweenService"), HttpService = game:GetService("HttpService"), TeleportService = game:GetService("TeleportService") }
 
 function Loader:Start()
     pcall(function()
         local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/thuanrb/Bloxfruitthuan/main/Modules/UI.lua"))()
-        local AutoFarm = loadstring(game:HttpGet("https://raw.githubusercontent.com/thuanrb/Bloxfruitthuan/main/Modules/AutoFarm.lua"))()
-        local Combat = loadstring(game:HttpGet("https://raw.githubusercontent.com/thuanrb/Bloxfruitthuan/main/Modules/Combat.lua"))()
-        local Performance = loadstring(game:HttpGet("https://raw.githubusercontent.com/thuanrb/Bloxfruitthuan/main/Modules/Performance.lua"))()
-        local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/thuanrb/Bloxfruitthuan/main/Modules/ESP.lua"))()
-        local ServerHop = loadstring(game:HttpGet("https://raw.githubusercontent.com/thuanrb/Bloxfruitthuan/main/Modules/ServerHop.lua"))()
-
         if UI then UI:Init(self) end
-        if Performance then Performance:Init(self) end
-        if AutoFarm then AutoFarm:Init(self) end
-        if Combat then Combat:Init(self) end
-        if ESP then ESP:Init(self) end
-        if ServerHop then ServerHop:Init(self) end
     end)
 end
 
