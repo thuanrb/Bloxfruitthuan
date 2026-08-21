80924b22592edda8ac355ee80973f4a6079125da
thuanrb <khadza2k45@gmail.com>
2026-08-21 07:34:23 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -15,11 +15,9 @@ Loader.Config = {
 
 function Loader:Start()
     pcall(function()
-        -- Load UI
         local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/thuanrb/Bloxfruitthuan/main/Modules/UI.lua"))()
         if UI then UI:Init(self) end
 
-        -- Load Performance (Tối ưu hiệu năng & Chống AFK)
         local Performance = loadstring(game:HttpGet("https://raw.githubusercontent.com/thuanrb/Bloxfruitthuan/main/Modules/Performance.lua"))()
         if Performance then Performance:Init(self) end
     end)
