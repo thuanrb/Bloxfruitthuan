fd1502d8f7587b0f800481940dd07147de15380d
thuanrb <khadza2k45@gmail.com>
2026-08-21 08:20:35 +0700

    Update Performance.lua

--- Modules/Performance.lua
+++ Modules/Performance.lua
@@ -1,4 +1,3 @@
--- Modules/Performance.lua
 local Performance = {}
 
 function Performance:Init(Loader)
@@ -6,20 +5,33 @@ function Performance:Init(Loader)
     local Workspace = game:GetService("Workspace")
     local Players = game:GetService("Players")
     local LocalPlayer = Players.LocalPlayer
+    local Terrain = Workspace:FindFirstChildOfClass("Terrain")
 
     task.spawn(function()
-        while task.wait(0.5) do
+        while task.wait(1) do
             if Loader.Config.BoostFPS then
                 pcall(function()
                     Lighting.GlobalShadows = false
                     Lighting.FogEnd = 9e9
+                    Lighting.Brightness = 1
                     settings():GetService("RenderSettings").RenderingEnabled = true
+                    
+                    if Terrain then
+                        Terrain.WaterWaveSize = 0
+                        Terrain.WaterWaveSpeed = 0
+                        Terrain.WaterReflectance = 0
+                        Terrain.WaterTransparency = 0
+                    end
+
                     for _, v in pairs(Workspace:GetDescendants()) do
-                        if v:IsA("Part") or v:IsA("MeshPart") then
+                        if v:IsA("Part") or v:IsA("MeshPart") or v:IsA("UnionOperation") then
                             v.Material = Enum.Material.SmoothPlastic
                             v.Reflectance = 0
-                        elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+                        elseif v:IsA("Decal") or v:IsA("Texture") or v:IsA("ParticleEmitter") or v:IsA("Trail") or v:IsA("Fire") or v:IsA("Smoke") or v:IsA("Sparkles") then
                             v.Enabled = false
+                            if v:IsA("Decal") or v:IsA("Texture") then
+                                v.Transparency = 1
+                            end
                         end
                     end
                 end)
